f63d3f6fa480045ae6711167662c8fe3d307d680
Simon Woolf <simon@ably.io>
2015-06-23 10:28:32 +0100

    add a couple echoes to use-kb scripts

--- .useLaptopKb.sh
+++ .useLaptopKb.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
-setxkbmap gb -variant colemak && xmodmap ~/.XmodmapForLaptopKeyboard
-killall python2 && quicktile.py --daemonize &
+setxkbmap gb -variant colemak
+xmodmap ~/.XmodmapForLaptopKeyboard
+killall python2
+echo `ps x | grep python2`
+nohup /usr/local/bin/quicktile.py --daemonize &
+echo `ps x | grep python2`
--- .useTEK.sh
+++ .useTEK.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
-setxkbmap gb && xmodmap ~/.XmodmapForTrulyErgonomic
-killall python2 && quicktile.py --daemonize &
+setxkbmap gb
+xmodmap ~/.XmodmapForTrulyErgonomic
+killall python2
+echo `ps x | grep python2`
+nohup /usr/local/bin/quicktile.py --daemonize &
+echo `ps x | grep python2`
