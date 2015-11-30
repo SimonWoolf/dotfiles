5131ee7bdee6fdfd0ef6b3176a525d84edeb5789
Simon Woolf <simon@ably.io>
2015-11-30 10:20:57 +0000

    Screenscaling: add xrandr config

--- .useScreenScaling.sh
+++ .useScreenScaling.sh
@@ -4,3 +4,4 @@ xfconf-query -c xsettings -p /Xft/DPI -s 96
 xfconf-query -c xfce4-panel -p /panels/panel-0/size -s 25
 xfconf-query -c xfwm4 -p /general/title_font -s "DejaVu Sans Bold 10"
 sed -i 's/user_pref("layout.css.devPixelsPerPx", "1.[0-9]");/user_pref("layout.css.devPixelsPerPx", "1.0");/' .mozilla/firefox/fuevxjfl.default/prefs.js
+xrandr --output DP1 --pos 1600x0 # Dell monitor to the right of laptop. --right-of puts it at 3200, so 1600 gap
