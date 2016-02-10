2953ea410bacaca4d87e5ef7723bd5a3cd5a96c1
Simon Woolf <simon@ably.io>
2016-02-10 12:52:36 +0000

    add date to work pomodoro comment

--- pomodoro-finish.sh
+++ pomodoro-finish.sh
@@ -7,7 +7,7 @@ zenity --question --text "Submit pomodoro to beeminder?"
 
 case $? in
   0)
-    RESULT=`curl -X POST "https://www.beeminder.com/api/v1/users/coiley/goals/work/datapoints.json?auth_token=$BEEMINDER_TOKEN&value=1&comment=xfce4-timer"`
+    RESULT=`curl -X POST "https://www.beeminder.com/api/v1/users/coiley/goals/work/datapoints.json?auth_token=$BEEMINDER_TOKEN&value=1&comment=xfce4-timer-$(date +%H:%M)"`
     if [[ $? -ne 0 ]]; then
       zenity --error --text "Curl failed"
     elif [[ `echo $RESULT | jq '.status'` != *created* ]]; then
