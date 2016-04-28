4d3b9151570a6db62b6dbdcaa4a86fed861e4619
Simon Woolf <simon@ably.io>
2016-04-28 23:01:50 +0100

    Pocket/beeminder: better error checking

--- pocket-beeminder-update.sh
+++ pocket-beeminder-update.sh
@@ -3,12 +3,17 @@
 
 NUM_UNREAD=`curl -X POST "https://getpocket.com/v3/get" -d consumer_key=$POCKET_CONSUMER_KEY -d access_token=$POCKET_ACCESS_TOKEN -d detailType=simple | jq '.list | reduce .[] as $item (0; . + 1)'`
 
+if [[ $? -ne 0 ]]; then
+  DISPLAY=:0 zenity --error --text "Pocket curl / jq failed"
+  exit 1
+fi
+
 RESULT=`curl -X POST "https://www.beeminder.com/api/v1/users/coiley/goals/pocket/datapoints.json?auth_token=$BEEMINDER_TOKEN&value=$NUM_UNREAD&comment=cron-job-$(date +%H:%M)"`
 
 if [[ $? -ne 0 ]]; then
-  zenity --error --text "Curl failed"
+  DISPLAY=:0 zenity --error --text "Beeminder curl failed"
 elif [[ `echo $RESULT | jq '.status'` != *created* ]]; then
-  zenity --error --text "Error submitting beeminder: $RESULT"
+  DISPLAY=:0 zenity --error --text "Error submitting beeminder: $RESULT"
 else
   notify-send "Pocket beemind submitted: $NUM_UNREAD"
 fi
