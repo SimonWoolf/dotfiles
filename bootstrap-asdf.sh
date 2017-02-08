f58c60a062c41896fcbde8519b9bfee7b4d1e190
Simon Woolf <simon@ably.io>
2017-02-08 13:08:59 +0000

    rehash after asdf

--- bootstrap-asdf.sh
+++ bootstrap-asdf.sh
@@ -3,3 +3,6 @@ git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.2.1
 asdf plugin-add elixir https://github.com/asdf-vm/asdf-elixir.git
 asdf plugin-add erlang https://github.com/asdf-vm/asdf-erlang.git
 asdf plugin-add ruby https://github.com/asdf-vm/asdf-ruby.git
+
+# reload bash's command cache -- run this after installing versions
+hash -r
