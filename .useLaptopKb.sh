6f50cc6422553ca371c7940793bfcc79b980155e
Simon Woolf <simon@ably.io>
2017-03-30 14:13:41 +0100

    Add i3 config

--- .useLaptopKb.sh
+++ .useLaptopKb.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 setxkbmap gb -variant colemak
 xmodmap ~/.XmodmapForLaptopKeyboard
-killall python2
-echo `ps x | grep python2`
-nohup /usr/local/bin/quicktile.py --daemonize &
-echo `ps x | grep python2`
+#killall python2
+#echo `ps x | grep python2`
+#nohup /usr/local/bin/quicktile.py --daemonize &
+#echo `ps x | grep python2`
