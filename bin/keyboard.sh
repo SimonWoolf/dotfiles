98dbf104667f9617c635f871fb501b9b264ef020
Simon Woolf <simon@ably.io>
2017-04-01 15:13:26 +0100

    attempt hack for keyboard layout on laptop

---
+++ bin/keyboard.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+sleep 2
+setxkbmap gb -variant colemak
+xmodmap ~/.XmodmapForLaptopKeyboard
+sleep 2
+setxkbmap gb -variant colemak
+xmodmap ~/.XmodmapForLaptopKeyboard
+sleep 2
+setxkbmap gb -variant colemak
+xmodmap ~/.XmodmapForLaptopKeyboard
