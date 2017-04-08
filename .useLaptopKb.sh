3f987c2b5a64f16f50ba60988ff8438ae4f40217
Simon Woolf <simon@ably.io>
2017-04-08 10:03:20 +0100

    Allow ctrl-alt-bksp

--- .useLaptopKb.sh
+++ .useLaptopKb.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 setxkbmap gb -variant colemak
+setxkbmap -option "terminate:ctrl_alt_bksp"
 xmodmap ~/.XmodmapForLaptopKeyboard
 #killall python2
 #echo `ps x | grep python2`
--- .useTEK.sh
+++ .useTEK.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 setxkbmap gb
+setxkbmap -option "terminate:ctrl_alt_bksp"
 xmodmap ~/.XmodmapForTrulyErgonomic
 killall python2
 echo `ps x | grep python2`
