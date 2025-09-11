43df358c7e509c66e3f7df4f46022b1d518d07a9
Simon Woolf <simon.woolf@ably.com>
2025-09-11 09:40:32 +0100

    vim: disable avante

--- vim-init.lua
+++ vim-init.lua
@@ -426,48 +426,48 @@ require("lazy").setup({
         })
       end
     },
-    {
-      "yetone/avante.nvim",
-      event = "VeryLazy",
-      lazy = false,
-      opts = {
-        -- disable obnoxious hints in visual mode
-        hints = { enabled = false },
-      },
-      -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-      build = "make",
-      dependencies = {
-        "stevearc/dressing.nvim",
-        "nvim-lua/plenary.nvim",
-        "MunifTanjim/nui.nvim",
-        --- The below dependencies are optional,
-        "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
-        "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-        -- "zbirenbaum/copilot.lua", -- for providers='copilot'
-        {
-          -- support for image pasting
-          "HakonHarnes/img-clip.nvim",
-          event = "VeryLazy",
-          opts = {
-            default = {
-              embed_image_as_base64 = false,
-              prompt_for_file_name = false,
-              drag_and_drop = {
-                insert_mode = true,
-              },
-            },
-          },
-        },
-        {
-          -- Make sure to set this up properly if you have lazy=true
-          'MeanderingProgrammer/render-markdown.nvim',
-          opts = {
-            file_types = { "markdown", "Avante" },
-          },
-          ft = { "markdown", "Avante" },
-        },
-      },
-    },
+    -- {
+    --   "yetone/avante.nvim",
+    --   event = "VeryLazy",
+    --   lazy = false,
+    --   opts = {
+    --     -- disable obnoxious hints in visual mode
+    --     hints = { enabled = false },
+    --   },
+    --   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+    --   build = "make",
+    --   dependencies = {
+    --     "stevearc/dressing.nvim",
+    --     "nvim-lua/plenary.nvim",
+    --     "MunifTanjim/nui.nvim",
+    --     --- The below dependencies are optional,
+    --     "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+    --     "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+    --     -- "zbirenbaum/copilot.lua", -- for providers='copilot'
+    --     {
+    --       -- support for image pasting
+    --       "HakonHarnes/img-clip.nvim",
+    --       event = "VeryLazy",
+    --       opts = {
+    --         default = {
+    --           embed_image_as_base64 = false,
+    --           prompt_for_file_name = false,
+    --           drag_and_drop = {
+    --             insert_mode = true,
+    --           },
+    --         },
+    --       },
+    --     },
+    --     {
+    --       -- Make sure to set this up properly if you have lazy=true
+    --       'MeanderingProgrammer/render-markdown.nvim',
+    --       opts = {
+    --         file_types = { "markdown", "Avante" },
+    --       },
+    --       ft = { "markdown", "Avante" },
+    --     },
+    --   },
+    -- },
     {
     'nvim-telescope/telescope.nvim', branch = 'master',
       dependencies = {
